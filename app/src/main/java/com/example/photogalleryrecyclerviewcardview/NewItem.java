42920b2c3e47b24a01796ff4cafd88661233ce25
Adriana Larsson <adrianalarsson@Adrianas-MacBook-Air.local>
2019-05-20 14:11:47 +0200

    now you can load up images and change

--- app/src/main/java/com/example/photogalleryrecyclerviewcardview/NewItem.java
+++ app/src/main/java/com/example/photogalleryrecyclerviewcardview/NewItem.java
@@ -2,6 +2,7 @@ package com.example.photogalleryrecyclerviewcardview;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ public class NewItem extends AppCompatActivity {
 
     Item mItem = new Item();
 
+    ImageView imageViewEdit;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,11 @@ public class NewItem extends AppCompatActivity {
 
         final EditText editTextTitle = (EditText) findViewById(R.id.editTextTitle);
 
-        Button mImagePicker = (Button) findViewById(R.id.mImagePicker);
 
-        mImagePicker.setOnClickListener(new View.OnClickListener() {
+
+        imageViewEdit = (ImageView) findViewById(R.id. imageViewEdit);
+
+        imageViewEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -59,28 +65,30 @@ public class NewItem extends AppCompatActivity {
 
 
     }
+
     public void createNewNote(Item item){
 
 
         mItem = item;
     }
 
+    public void SelectPhotoMethod(View view) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }   
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        switch(requestCode) {
-            case 0:
-                if(resultCode == RESULT_OK){
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    //imageview.setImageURI(selectedImage);
-                }
 
-                break;
-            case 1:
-                if(resultCode == RESULT_OK){
-                   Uri selectedImage = imageReturnedIntent.getData();
-                    //imageview.setImageURI(selectedImage);
-                }
-                break;
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Uri selectedImage = imageReturnedIntent.getData();
+           imageViewEdit.setImageURI(selectedImage);
         }
     }
+
+
 }
